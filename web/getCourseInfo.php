18813f616a5207c78c905df8edc269323023c969
gaurav gupta <gaurav gupta>
2017-04-06 23:23:24 +0530

    Modify to fix chd issue

--- web/getCourseInfo.php
+++ web/getCourseInfo.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("lib/utility.class.php");
+//include_once("lib/utility.class.php");
 
 $q = $_GET["q"];
 /* @saurav Change here to fetch data using project_id for both developer and tester */
@@ -8,8 +8,8 @@ $pro_id = $_GET["pro_id"];
 
 include("config.php");
 
-$obj_utility = new Utility();
-$sql="SELECT id , course_title from tbl_functional_review WHERE project_id = '".$pro_id."'";
+//$obj_utility = new Utility();
+$sql="SELECT id, course_title from tbl_functional_review WHERE project_id = '".$pro_id."'";
 $result = mysql_query($sql) or die (mysql_error());
 $count = mysql_num_rows($result);
 $ary=array();
@@ -25,6 +25,6 @@ if($count==0)
 	}
 }
 
-echo $obj_utility->php_to_js($ary);
+echo json_encode($ary);
 mysql_close($con);
 ?> 
