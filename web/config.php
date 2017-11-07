eb1a3bb7710015247043bda24b8d59548d2daa23
Ankur Jain <smartyankur@gmail.com>
2017-11-07 11:08:54 +0000

    new server fixes

--- web/config.php
+++ web/config.php
@@ -1,7 +1,8 @@
 <?php	
-  $con = mysql_connect("localhost", "root", "");
+error_reporting(E_ERROR | E_PARSE); 
+  $con = mysql_connect("localhost", "radar", "thelevonad");
   if(!$con){
     die('Could not connect: ' . mysql_error());
   }
-  mysql_select_db("radarTesting") or die(mysql_error());  
+  mysql_select_db("radar") or die(mysql_error());  
 ?> 
